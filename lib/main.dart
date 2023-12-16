b5458f187aede8d2b3a75e5516679524b40384cf
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2023-12-16 22:13:13 +0700

    poppins fonts

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'WasteApp',
+              style: TextStyle(
+                fontFamily: "Poppins",
+              ),
             ),
             Text(
               '$_counter',
