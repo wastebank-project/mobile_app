b086a0159aeac84e6dc8f2782bd2c82e619e55f2
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-04-22 11:55:13 +0700

    feat: add image slider widget

--- lib/presentation/page/home_page/home_page.dart
+++ lib/presentation/page/home_page/home_page.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:waste_app/presentation/page/home_page/methods/username.dart';
 import 'package:waste_app/presentation/page/home_page/methods/logout.dart';
+import 'package:waste_app/presentation/widgets/images_slider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,38 +12,43 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Username(context),
-                LogoutButton(context),
-              ],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Username(context),
+                  LogoutButton(context),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(height: 50),
-          const Text(
-            'Selamat Datang!',
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            '"WasteApp" adalah aplikasi inovatif '
-            'yang dirancang untuk memberdayakan pengelola bank sampah dalam '
-            'menjaga lingkungan dengan menyediakan fitur deteksi sampah dan '
-            'pencatatan transaksi sampah di Bank Sampah.'
-            '\n\nAplikasi ini menggunakan teknologi pengenalan gambar dan kecerdasan buatan untuk '
-            'mengidentifikasi jenis sampah dan rekomendasi daur ulang sampah.',
-            textAlign: TextAlign.justify,
-            style: TextStyle(fontSize: 15, height: 1.7),
-          )
-        ],
-      )),
+            const SizedBox(height: 20),
+            const Text(
+              'Selamat Datang!',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text(
+              '"WasteApp" adalah aplikasi inovatif '
+              'yang dirancang untuk memberdayakan pengelola bank sampah dalam '
+              'menjaga lingkungan dengan menyediakan fitur deteksi sampah dan '
+              'pencatatan transaksi sampah di Bank Sampah.'
+              '\n\nAplikasi ini menggunakan teknologi pengenalan gambar dan kecerdasan buatan untuk '
+              'mengidentifikasi jenis sampah dan rekomendasi daur ulang sampah.',
+              textAlign: TextAlign.justify,
+              style: TextStyle(fontSize: 15, height: 1.7),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            const ImageSlider(),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/presentation/widgets/images_slider.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class ImageSlider extends StatefulWidget {
+  const ImageSlider({super.key});
+
+  @override
+  State<ImageSlider> createState() => _ImageSliderState();
+}
+
+class _ImageSliderState extends State<ImageSlider> {
+  final List<String> images = [
+    'assets/png/cibunut1.png',
+    'assets/png/cibunut2.png',
+    'assets/png/cibunut3.png',
+    'assets/png/cibunut4.png',
+  ];
+
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        CarouselSlider.builder(
+          itemCount: images.length,
+          itemBuilder: (BuildContext context, int index, _) {
+            return Image.asset(
+              images[index],
+              fit: BoxFit.cover,
+            );
+          },
+          options: CarouselOptions(
+            aspectRatio: 16 / 9,
+            viewportFraction: 1,
+            onPageChanged: (index, _) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
+        ),
+        SizedBox(
+          height: 40,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+              images.length,
+              (index) => _buildDot(index),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildDot(int index) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      height: 10,
+      width: _currentIndex == index ? 20 : 10,
+      decoration: BoxDecoration(
+        color: _currentIndex == index ? Color(0xFF7FB77E) : Colors.grey,
+        borderRadius: BorderRadius.circular(7),
+      ),
+    );
+  }
+}
