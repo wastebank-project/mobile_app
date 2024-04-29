a04ace71a685fcf53da803f98ab7dff1f3889f7a
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-04-29 16:19:19 +0700

    feat: add logout onpressed

--- lib/presentation/page/home_page/methods/logout.dart
+++ lib/presentation/page/home_page/methods/logout.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:waste_app/presentation/page/login_page/login_screen.dart';
 
 Widget LogoutButton(BuildContext context) => IconButton(
-      onPressed: () {},
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => LoginScreen(),
+          ),
+        );
+      },
       icon: Icon(Icons.logout),
       iconSize: 30,
     );
