edb35154854bdbc00bc7885ed4e377d35100e7fa
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-05-28 16:05:45 +0700

    adjustment welcoming page

--- lib/presentation/page/welcoming_page/welcoming_page.dart
+++ lib/presentation/page/welcoming_page/welcoming_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:waste_app/presentation/page/login_page/login_screen.dart';
 import 'package:waste_app/presentation/page/register_page/register_page.dart';
 
@@ -15,70 +14,17 @@ class WelcomingPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          SizedBox(height: 150),
-          SvgPicture.asset("assets/svg/log-reg.svg"),
-          SizedBox(height: 60),
+          SizedBox(height: 180),
+          Image.asset("assets/png/WasteApp.png"),
+          SizedBox(height: 5),
           const Center(
             child: Column(
               children: [
-                Text(
-                  "WasteApp",
-                  style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.w700,
-                      color: Color(0xFF7FB77E)),
-                ),
-                SizedBox(height: 5),
-                Text("Waste Bank Administration App"),
+                Text("Aplikasi Bank Sampah Pintar"),
               ],
             ),
           ),
-          SizedBox(height: 40),
-          TextButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (context, animation, secondaryAnimation) =>
-                      RegisterScreen(),
-                  transitionsBuilder:
-                      (context, animation, secondaryAnimation, child) {
-                    const begin = Offset(1.0, 0.0);
-                    const end = Offset.zero;
-                    const curve = Curves.easeInOut;
-
-                    var tween = Tween(begin: begin, end: end)
-                        .chain(CurveTween(curve: curve));
-
-                    var offsetAnimation = animation.drive(tween);
-
-                    return SlideTransition(
-                      position: offsetAnimation,
-                      child: child,
-                    );
-                  },
-                  transitionDuration: const Duration(milliseconds: 1000),
-                ),
-              );
-            },
-            style: ButtonStyle(
-              backgroundColor:
-                  MaterialStateProperty.all(const Color(0xFF7FB77E)),
-              shape: MaterialStateProperty.all(
-                const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-            child: const Text("Create New Account",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600)),
-          ),
-          SizedBox(height: 10),
+          SizedBox(height: 70),
           TextButton(
             onPressed: () {
               Navigator.pushReplacement(
@@ -107,15 +53,60 @@ class WelcomingPage extends StatelessWidget {
               );
             },
             style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(const Color(0xFF7ABA78)),
+              shape: MaterialStateProperty.all(
+                const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            child: const Text("Masuk",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600)),
+          ),
+          SizedBox(height: 10),
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) =>
+                      RegisterScreen(),
+                  transitionsBuilder:
+                      (context, animation, secondaryAnimation, child) {
+                    const begin = Offset(1.0, 0.0);
+                    const end = Offset.zero;
+                    const curve = Curves.easeInOut;
+
+                    var tween = Tween(begin: begin, end: end)
+                        .chain(CurveTween(curve: curve));
+
+                    var offsetAnimation = animation.drive(tween);
+
+                    return SlideTransition(
+                      position: offsetAnimation,
+                      child: child,
+                    );
+                  },
+                  transitionDuration: const Duration(milliseconds: 1000),
+                ),
+              );
+            },
+            style: ButtonStyle(
               side: MaterialStateProperty.all(
-                  const BorderSide(color: Color(0xFF7FB77E), width: 2.5)),
+                  const BorderSide(color: Color(0xFF7ABA78), width: 2.5)),
               shape: MaterialStateProperty.all(RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10),
               )),
             ),
-            child: const Text("I Have Account",
+            child: const Text("Belum punya akun",
                 style: TextStyle(
-                    color: Color(0xFF7FB77E),
+                    color: Color(0xFF7ABA78),
                     fontSize: 15,
                     fontWeight: FontWeight.w600)),
           ),
