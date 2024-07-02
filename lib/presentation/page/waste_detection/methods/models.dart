9576938c211ea9fd6a8ca75b2acc7e6182fa7240
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-07-02 18:04:02 +0700

    feat: recomendation

--- lib/presentation/page/waste_detection/methods/models.dart
+++ lib/presentation/page/waste_detection/methods/models.dart
@@ -1,12 +1,158 @@
 class Models {
   final String label;
-  final String youtubeLink;
+  final List<Map<String, String>> youtubeRecommendations;
 
-  Models({required this.label, required this.youtubeLink});
+  Models({required this.label, required this.youtubeRecommendations});
 
-  static Map<String, String> recommendationMap = {
-    "Galon": "https://www.youtube.com/watch?v=example_galon",
-    "Botol Plastik": "https://www.youtube.com/watch?v=example_botol_plastik",
+  static Map<String, List<Map<String, String>>> recommendationMap = {
+    "Galon": [
+      {
+        "heading": "Komposter Sedehana dari Galon Air Mineral",
+        "link": "https://youtu.be/5LfeaE0XFxA?si=K2bCz1hTS04lkp9d"
+      },
+      {
+        "heading":
+            "7 Ide kreatif dari galon le minerale bekas ini PAS BANGET BUAT RUMAHMU",
+        "link": "https://youtu.be/5RF6xWmxCmA?si=gOLzPBl35Fbmg4At"
+      },
+    ],
+    "Botol Plastik": [
+      {
+        "heading": "Pemanfaatan sampah botol plastik",
+        "link": "https://youtu.be/qfOTfIVJw-s?si=pgZIjeE4pBZTgSHW"
+      },
+      {
+        "heading": "Pengolahan Limbah Plastik dengan Mesin Pencacah Plastik",
+        "link": "https://youtu.be/dOoR-YFzFQo?si=58l0eVt2G_64f6rn"
+      }
+    ],
+    "Botol Kaca": [
+      {
+        "heading": "SETELAH NONTON PASTI SEMUA CARI BOTOL KACA BEKAS",
+        "link": "https://youtu.be/mOiuMTP8HJM?si=0FSnH0xJLYAXGJ91",
+      },
+      {
+        "heading": "Glass Bottle Recycling - Factory Installation",
+        "link": "https://youtu.be/ZEVq0oxL4_A?si=YKwHRB9TTXj19hZS",
+      }
+    ],
+    "Pecahan Kaca": [
+      {
+        "heading": "Daur Ulang Limbah Pecahan Kaca Menjadi Pot Bunga",
+        "link": "https://youtu.be/Ny8Af_ADvtg?si=Q54LgF5QksxmP55T"
+      },
+      {
+        "heading": "DAUR ULANG LIMBAH KACA DAN BELING #brammilano",
+        "link": "https://youtu.be/QtQPEuja_1k?si=r6g7s9bBJN6gbjNx"
+      },
+    ],
+    "Tutup Galon": [
+      {
+        "heading": "DAUR ULANG TUTUP GALON BEKAS - BANK SAMPAH SARIMULYA",
+        "link": "https://youtu.be/dNIEzFB9XD8?si=HBGMuF7Ap5QmYRZC"
+      },
+      {
+        "heading": "Keranjang dari tutup Aqua galon",
+        "link": "https://youtu.be/WGbAHRrLQIg?si=Q4SD61D7SoyR_M4Z"
+      },
+    ],
+    "Koran": [
+      {
+        "heading": "Kreasi Limbah Kertas yang Menguntungkan | Halo Indonesia",
+        "link": "https://youtu.be/tAbJ5WUFa_Y?si=ykS5sX2Bn_ojGzGM"
+      },
+      {
+        "heading":
+            "Cara membuat kreasi kerajinan keranjang dari kertas bekas, koran bekas, majalah bekas",
+        "link": "https://youtu.be/7h7Xh-c4KJ0?si=Azj-IHXpyMeOKwcj"
+      },
+    ],
+    "Kemasan Plastik": [
+      {
+        "heading": "PENGOLAHAN SAMPAH PLASTIK KEMASAN SACHET",
+        "link": "https://www.youtube.com/watch?v=kNpxiuLrAwQ"
+      },
+      {
+        "heading":
+            "Daur ulang sampah plastik bungkus snack menjadi tempat pensil || Recycle from plastic snack",
+        "link": "https://www.youtube.com/watch?v=MJd3bo_XRaU"
+      },
+    ],
+    "Gelas Plastik": [
+      {
+        "heading": "Pengolahan Sampah Anorganik Gelas Plastik",
+        "link": "https://youtu.be/onfJudUu7UE?si=pFiEQxnYshpAuQU3"
+      },
+      {
+        "heading": "Daur Ulang Sampah Botol & Gelas Plastik",
+        "link": "https://youtu.be/uqc6Q4bLq2E?si=6ImcJlQWpssM5sAp"
+      },
+    ],
+    "Kaleng": [
+      {
+        "heading":
+            "KARYA UNIK PALING BERKELAS KREATIF DARI LIMBAH KALENG MINUMAN",
+        "link": "https://youtu.be/jue0SHLuxuY?si=30Ao4zo2n-YcysaL"
+      },
+      {
+        "heading": "Eksperimen Cara Daur Ulang Kaleng Aluminum Bekas Aluminium",
+        "link": "https://youtu.be/ajEECRjRQEE?si=m-SYApqKE9JsjnKB"
+      },
+    ],
+    "Kantong Plastik": [
+      {
+        "heading":
+            "KEREN PARAH, DAUR ULANG KANTONG PLASTIK JADI 100% KANTONG PLASTIK LAGI !",
+        "link": "https://youtu.be/MZ5jcNdAOEM?si=WqKXhMJAguJcaeJY"
+      },
+      {
+        "heading": "Daur Ulang Sampah Kantong Plastik",
+        "link": "https://youtu.be/Rk0QurFdLK4?si=Ism_EB9_iMqtjWLB"
+      },
+    ],
+    "Kantong Semen": [
+      {
+        "heading":
+            "GTK Creative Camp 2020 Daur ulang karung semen menjadi dompet",
+        "link": "https://youtu.be/B5tTKh3KtPE?si=zMcSR8OQMyfoatsP"
+      },
+      {
+        "heading": "PAPER BAG KERTAS SEMEN",
+        "link": "https://youtu.be/H1xxHoj1zpo?si=-xZT-yAn4ueG9DCk"
+      },
+    ],
+    "Kardus": [
+      {
+        "heading":
+            "Puluhan Juta dari kardus bekas❗ menjadi barang kebutuhan Bernilai Ekonomi",
+        "link": "https://youtu.be/lng70rxNRe8?si=2geOnEU9NHttN7zW"
+      },
+      {
+        "heading": "Daur ulang kardus bekas ( program inovasi desa)",
+        "link": "https://www.youtube.com/watch?v=Q3qVajGnqp8"
+      },
+    ],
+    "Toples Kaca": [
+      {
+        "heading":
+            "DIY Lampu Dinding Modern Unik dan Mewah dari Botol Kaca dan Pipa PVC bekas",
+        "link": "https://www.youtube.com/watch?v=1OjJuwCgpms"
+      },
+      {
+        "heading": "DIY / 8 Best Ideas from recycled Glass jars.",
+        "link": "https://www.youtube.com/watch?v=n4vlGT9-Zng"
+      },
+    ],
+    "Kertas Bekas": [
+      {
+        "heading": "Cara Daur Ulang Kertas Bekas Dirumah",
+        "link": "https://www.youtube.com/watch?v=Z1DER7uUvu8"
+      },
+      {
+        "heading": "Pengolahan Limbah Kertas Menjadi Batako",
+        "link": "https://www.youtube.com/watch?v=WdZO2c2fUXk"
+      },
+    ],
     // Add more mappings here
   };
 
@@ -14,7 +160,10 @@ class Models {
     return labels.toSet().map((label) {
       return Models(
         label: label,
-        youtubeLink: recommendationMap[label] ?? "https://www.youtube.com",
+        youtubeRecommendations: recommendationMap[label] ??
+            [
+              {"heading": "Default YouTube", "link": "https://www.youtube.com"}
+            ],
       );
     }).toList();
   }
