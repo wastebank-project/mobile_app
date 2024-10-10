27cbe7b92695f738b9411843778d399255f373ff
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-10-10 16:44:15 +0700

    feat: feature of saving history

--- lib/presentation/page/customers_page/methods/list_customers/detail_customers.dart
+++ lib/presentation/page/customers_page/methods/list_customers/detail_customers.dart
@@ -1,115 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:waste_app/domain/customers.dart';
-import 'package:waste_app/presentation/page/customers_page/methods/list_customers/edit_customers.dart';
 
-class DetailCustomer extends StatelessWidget {
+class DetailCustomer extends StatefulWidget {
   final Map<String, dynamic> nasabah;
 
   const DetailCustomer({Key? key, required this.nasabah}) : super(key: key);
 
-// HAPUS CUSTOMER
-  void _deleteCustomer(BuildContext context) async {
-    bool? confirmed = await showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          icon: const Icon(
-            Icons.warning_amber,
-            size: 50,
-          ),
-          iconColor: Colors.red,
-          title: const Text(
-            'PENGHAPUSAN NASABAH',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
-            ),
-          ),
-          content: const Text(
-            'Setelah dihapus, anda tidak dapat memulihkan nasabah ini',
-            style: TextStyle(fontSize: 15),
-          ),
-          actions: [
-            SizedBox(
-              width: 120,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(true);
-                },
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all(
-                    const Color(0xffE66776),
-                  ),
-                  shape: WidgetStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-                child: const Text(
-                  'Hapus Data',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(width: 5),
-            SizedBox(
-              width: 120,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(false);
-                },
-                style: ButtonStyle(
-                  side: WidgetStateProperty.all(
-                      const BorderSide(color: Colors.black, width: 2.5)),
-                  shape: WidgetStateProperty.all(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  )),
-                ),
-                child: const Text(
-                  'Batal',
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        );
-      },
-    );
-// FUNGSI PEMANGGILAN API TERHADAP KODE DIATAS
-    if (confirmed == true) {
-      EasyLoading.show(status: 'loading');
-      try {
-        await Customer().deleteCustomer(nasabah['id']);
-        // ignore: use_build_context_synchronously
-        Navigator.pop(context, true);
-      } catch (e) {
-        // ignore: use_build_context_synchronously
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Failed to delete customer: $e')),
-        );
-      }
-    }
-    EasyLoading.dismiss();
-  }
+  @override
+  _DetailCustomerState createState() => _DetailCustomerState();
+}
 
-// EDIT NASABAH
-  void _editCustomer(BuildContext context) async {
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => EditCustomerScreen(nasabah: nasabah),
-      ),
-    );
-    // Buat Refresh list
-    if (result == true) {
-      // ignore: use_build_context_synchronously
-      Navigator.pop(context, true);
+class _DetailCustomerState extends State<DetailCustomer> {
+  List<dynamic> _history = [];
+  bool _isLoading = false;
+
+  // Fetch customer history from /tabung API
+  Future<void> _fetchCustomerHistory() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      List<dynamic> history =
+          await Customer().getCustomerHistory(widget.nasabah['name']);
+      setState(() {
+        _history = history;
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to fetch history: $e')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 
@@ -129,7 +53,7 @@ class DetailCustomer extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 50),
+            const SizedBox(height: 20),
             const Text(
               'Nama',
               style: TextStyle(
@@ -137,93 +61,45 @@ class DetailCustomer extends StatelessWidget {
                 fontSize: 20,
               ),
             ),
-            const SizedBox(height: 5),
-            Text('${nasabah['name']}'),
+            Text(widget.nasabah['name']),
             const SizedBox(height: 20),
-            const Text(
-              'Email',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            Text('${nasabah['email']}'),
-            const SizedBox(height: 20),
-            const Text(
-              'Alamat',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(height: 5),
-            Text('${nasabah['address']}'),
-            const SizedBox(height: 20),
-            const Text(
-              'Nomor Telepon',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(height: 5),
-            Text('${nasabah['phoneNumber']}'),
-            const SizedBox(height: 75),
+            // Add a button to fetch the history
             Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 150,
-                    child: TextButton(
-                      onPressed: () {
-                        _editCustomer(context);
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all(
-                          const Color(0xFF7ABA78),
-                        ),
-                        shape: WidgetStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                      child: const Text(
-                        'Edit Data',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 20),
-                  SizedBox(
-                    width: 150,
-                    child: TextButton(
-                      onPressed: () => _deleteCustomer(context),
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all(
-                          const Color(0xffE66776),
-                        ),
-                        shape: WidgetStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                      child: const Text(
-                        'Hapus Data',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+              child: ElevatedButton(
+                onPressed: _fetchCustomerHistory,
+                child: _isLoading
+                    ? const CircularProgressIndicator(color: Colors.white)
+                    : const Text('Lihat History'),
               ),
+            ),
+            const SizedBox(height: 20),
+            // Display history if fetched
+            if (_history.isNotEmpty)
+              const Text('Riwayat Transaksi:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+            Expanded(
+              child: _history.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: _history.length,
+                      itemBuilder: (context, index) {
+                        final historyItem = _history[index];
+                        return ListTile(
+                          title: Text('Date: ${historyItem['date']}'),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                  'Total Balance: ${historyItem['totalBalance']}'),
+                              ...historyItem['deposits'].map<Widget>((deposit) {
+                                return Text(
+                                    'Deposit Amount: ${deposit['amount']}');
+                              }).toList(),
+                            ],
+                          ),
+                        );
+                      },
+                    )
+                  : const Text('No history available.'),
             ),
           ],
         ),
